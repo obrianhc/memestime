81b5205dfe28a0d940a3fc70dd33ec5bf5d7fec8
luisfya <luisfyaplus@gmail.com>
2016-02-05 12:43:52 -0600

    Modificaciones parciales al archivo upload

--- upload.php
+++ upload.php
@@ -5,16 +5,24 @@
 </head>
 
 <body>
-
+<?php
+	require_once('clases/uploadAction.php')			
+?>
 <label >Subir meme</label><br>
+	<form action = "upload.php" method = "post" enctype="multipart/form-data">
+		<input type="file" id="btnSeleccionar" value="Elegir archivo"><br>
+		<input type="text" id="txtNombre"><br>
+		<button type="button" id="btnSubir">subir archivo</button>
 
-    <input type="file" id="btnSeleccionar" value="Elegir archivo">
-    <input type="text" id="txtNombre"><br>
-    <button type="button" id="btnSubir">subir archivo</button>
+		<?php
+			$dir = "tmp/"
+			$fileName = $dir . basename($_FILES["btnSeleccionar"]["name"])
+		?>
 
-	<div id=respuesta>
-		<!-- Aqui colocar mensaje de respuesta, si el archivo se subio con exito, o si hubo algun error-->
-	</div>
+		<div id=respuesta>
+			<!-- Aqui colocar mensaje de respuesta, si el archivo se subio con exito, o si hubo algun error-->
+		</div>
+	</form>
     
 
 </body>
