528681b3c463229dfe28731bf9b462379d88111c
Edrson <ederson.ramirez87@gmail.com>
2016-02-05 11:14:23 -0600

    se agrego archivo upload con la interfaz minima funcional para subir archivo

---
+++ upload.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Upload</title>
+
+</head>
+
+<body>
+
+<label >Subir meme</label><br>
+
+    <button type="button" id="btnSeleccionar">Elejir archivo</button>
+    
+    <input type="text" id="txtNombre"><br>
+    <button type="button" id="btnSubir">subir archivo</button>
+
+	<div id=respuesta>
+		<!-- Aqui colocar mensaje de respuesta, si el archivo se subio con exito, o si hubo algun error-->
+	</div>
+    
+
+</body>
+</html>
