ada408ca4feb8e58fffa714344e6abe5f7b5503f
tonyusac <lamevillcor13@gmail.com>
2016-02-05 12:31:43 -0600

    insert mongodb y php

--- clases/testUpload.php
+++ clases/testUpload.php
@@ -1,30 +1,17 @@
 <?php
 class testUpload{
-		function upload($filename, $filetype, $title){
-			// Datos pertinentes al archivo para subir			
-			$target_dir = "clases/";
-			$datetime = time();
-			$md5_filename = md5($filename);
-			$target_file = $target_dir . $md5_filename . time();
-			// Colocamos la extension adecuada al archivo para subir
-			if($filetype == "jpg"){
-				$target_file = $target_file . ".jpg";
-			} else if($filetype == "png"){
-				$target_file = $target_file . ".png";
-			} else if($filetype == "gif"){
-				$target_file = $target_file . ".gif";
-			}else if($filetype == "bmp"){
-				$target_file = $target_file . ".bmp";
-			}else {
-				return false;
-			}
+
+		function upload($tipoImagen, $nombreMeme, $nombreUsuario){
+			// Datos pertinentes al archivo para subir
+			$md5NombreImagen = md5($nombreMeme . time());
+			$url = "images.memestime.com/". $md5NombreImagen . '.' . $tipoImagen;
 			// Verificamos que realmente subiremos un archivo
 			if(!$isset){
 				return false;
 			}
 			// Ahora subiremos el archivo
 			if (move_uploaded_file($tempname, $target_file)) {
-				$status = $this->insert_file($userid, $title, $target_file, $description);
+				$status = $this->insert_file($nombreUsuario, $nombreMeme, $url);
 				if($status)
 		        	return true;
 		        else
@@ -33,16 +20,21 @@ class testUpload{
 		    	return false;
 		    }
 		}
-		function insert_file($userid, $title, $path, $description){
-			include('dbm.php');
-			$query = "INSERT INTO archivo (id_usuario, nombre, descripcion, ruta) 
-						VALUES ($userid, '$title', '$description', '$path');";
-			$data = new DataBase();
-			$data->open();
-			$result = mysqli_query($data->get_connect(), $query) 
-				or die ('Error insertando registro: ' . mysqli_error($data->get_connect()));
-			$data->close();
-			if($result)
+
+		function insert_file($nombreUsuario, $nombreMeme, $url){
+
+			$conexion = new Mongo('192.168.122.188');
+			$baseDatos = $conexion->selectDB('memestime');
+			$coleccion = $baseDatos->selectCollection('imagenes');
+			$registro = array(
+				'usuario' => $nombreUsuario,
+				'nombreImagen' => $nombreMeme,
+				'fecha' => new MongoDate(),
+				'url' => $url
+			);
+			$resultado = $coleccion->insert($registro) 
+				or die ('Error insertando registro en mongodb');
+			if($resultado)
 				return true;
 			else
 				return false;
