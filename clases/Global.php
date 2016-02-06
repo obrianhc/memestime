6deaca5c6ed312760551fd1c36e6e05acd4bdbbc
luisfya <luisfyaplus@gmail.com>
2016-02-05 19:30:06 -0600

    Subida de imagenes, temporalmente en servidor web y luego al servidor ftp, falta conexión a mongo

--- clases/Global.php
+++ clases/Global.php
@@ -1,3 +1,37 @@
 <?php
-				
+	class G{
+		private $directorioTmp;
+		private $direccionMongo;
+		private $ftpServer;
+		private $ftpUserName;
+		private $ftpUserPass;
+
+		function __construct(){
+			$this->directorioTmp = "tmp/";
+			$this->direccionMongo = "192.168.122.188";
+
+			$this->ftpServer = "192.168.124.189";
+			$this->ftpUserName = "anonymous";
+			$this->ftpUserPass = "";
+		}
+
+		function getDirectorioTmp(){
+			return $this->directorioTmp;
+		}
+		function getDireccionMongo(){
+			return $this->direccionMongo;
+		}
+		
+		function getFtpServer(){
+			return $this->ftpServer;	
+		}
+		function getFtpUserName(){
+			return $this->ftpUserName;	
+		}
+		function getFtpUserPass(){
+			return $this->ftpUserPass;	
+		}
+
+
+	}			
 ?>
--- upload.php
+++ upload.php
@@ -4,20 +4,31 @@
 </head>
 <body>
 <?php
-	require_once('clases/uploadAction.php')			
+	require_once('clases/uploadAction.php');
+	$strRepuesta = "";	
 ?>
 <label >Subir meme</label><br>
 	<form action = "upload.php" method = "post" enctype="multipart/form-data">
-		<input type="file" id="btnSeleccionar" value="Elegir archivo"><br>
-		<input type="text" id="txtNombre"><br>
-		<button type="button" id="btnSubir">subir archivo</button>
+		<input type="file" name = "btnSeleccionar" id="btnSeleccionar" value="Elegir archivo"><br>
+		<label >Nombre: </label><input type="text" id="txtNombre" name = "txtNombre"><br>
+		<button type="submit" id="btnSubir" name = "btnSubir">subir archivo</button>
 
 		<?php
-			$dir = "tmp/"
-			$fileName = $dir . basename($_FILES["btnSeleccionar"]["name"])
+			$nombreArchivo = basename($_FILES["btnSeleccionar"]["name"]);
+			$tipoArchivo =  pathinfo($nombreArchivo,PATHINFO_EXTENSION);
+			$nombreTemporal = $_FILES["btnSeleccionar"]["tmp_name"];
+			$tamanhoArchivo = $_FILES["btnSeleccionar"]["size"];
+
+			if(isset($_POST['btnSubir'])){
+				$arch = new memestimeArchivo();
+				$status = $arch->upload("luisfya", $nombreArchivo, $_POST["txtNombre"], $tipoArchivo, $tamanhoArchivo, isset($_POST["submit"]), $nombreTemporal, $strRepuesta);
+			}
 		?>
 
 		<div id=respuesta>
+			<?php
+				echo $strRepuesta
+			?>
 			<!-- Se ha cargado correctamente el archivo -->
 			<!-- Ha ocurrido un error al cargar el archivo -->
 		</div>
